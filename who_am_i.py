5f9f176011089c721daa80e925495aea7841911f
zhanghongjie <zhanghongjie@momenta.ai>
2022-08-24 15:16:43 +0800

    add branch name

--- who_am_i.py
+++ who_am_i.py
@@ -1,3 +1,9 @@
 name = 'test'
 print('this is a test branch!')
 branch_name = 'test branch'
+
+
+def add(a: int, b: int, env='test'):
+    if not isinstance(a, int) or not isinstance(b, int):
+        return 0
+    return a + b
